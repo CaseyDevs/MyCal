5918caad2b79087ad9aa92dcd06513a0a4ff3e73
CaseyDevs <CaseyDevs5@gmail.com>
2026-08-16 21:26:30 +0100

    migration: add base entity 'CreatedAt' columns to foods / logs

---
+++ MyCal.Application/Migrations/20260816202014_AddBaseEntityProperties.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace MyCal.Application.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddBaseEntityProperties : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "FoodLog",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValueSql: "CURRENT_TIMESTAMP");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Food",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValueSql: "CURRENT_TIMESTAMP");
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "FoodLog");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Food");
+        }
+    }
+}
